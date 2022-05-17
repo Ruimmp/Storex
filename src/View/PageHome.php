1215949e4c2fdc76900d94b3bb216a7a714d8afa
cpnv-Nuno <79204685+cpnv-Nuno@users.noreply.github.com>
2022-05-17 14:18:41 +0200

    Update PageHome.php

--- src/View/PageHome.php
+++ src/View/PageHome.php
@@ -8,6 +8,7 @@
 
 $title = "Accueil Storex";
 ob_start();
+$rows=0; // Column count
 ?>
 
     <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll"
@@ -20,103 +21,41 @@ ob_start();
         </form>
     </div>
 
+    <article>
+        <header>
 
-    <div class="container-fluid tm-container-content tm-mt-60">
-        <div class="row mb-4">
-            <h2 class="col-6 tm-text-primary">Catégories</h2>
-        </div>
+            <div class="yox-view">
 
-        <div class="row tm-gallery">
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/woman.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Femmes</h2>
-                        <a href="./View/PageWomenArticles.php">View more</a>
-                    </figcaption>
-                </figure>
+                <?php foreach ($ArticlesResult as $result ) : ?>
+                    <?php $rows++; ?>
+                    <?php if ($rows%4) : // tests to have 4 items / line ?>
+                        <div class="row-fluid">
+                        <ul class="thumbnails">
+                        <?php $rows=0;?>
+                    <?php endif ?>
+
+                    <li class="span3">
+                        <div class="thumbnail">
+                            <img src="<?= $result['inputImage']; ?>" alt="<?= $result['id']; ?>">
+                            <div class="caption">
+                                <h2><?= $result['inputName']; ?></h2>
+                                <h3><?= $result['inputPrice']; ?></h3>
+                                <p><?= $result['inputDescription']; ?></p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <?php if ($rows%4) :?>
+                        </ul>
+                        </div>
+                    <?php endif ?>
+                <?php endforeach ?>
+
             </div>
+        </header>
+    </article>
+    <hr/>
 
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/man.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Hommes</h2>
-                        <a href="photo-detail.html">View more</a>
-                    </figcaption>
-                </figure>
-            </div>
-
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/kids.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Enfants</h2>
-                        <a href="photo-detail.html">View more</a>
-                    </figcaption>
-                </figure>
-            </div>
-
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/access.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Accessoires</h2>
-                        <a href="photo-detail.html">View more</a>
-                    </figcaption>
-                </figure>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="container-fluid tm-container-content tm-mt-60">
-        <div class="row mb-4">
-            <h2 class="col-6 tm-text-primary">Utilisateurs</h2>
-        </div>
-
-        <div class="row tm-gallery">
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/img-03.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Femmes</h2>
-                        <a href="index.php?action=WomenCategory">View more</a>
-                    </figcaption>
-                </figure>
-            </div>
-
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/img-03.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Hommes</h2>
-                        <a href="index.php?action=ManCategory">View more</a>
-                    </figcaption>
-                </figure>
-            </div>
-
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/img-03.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Enfants</h2>
-                        <a href="index.php?action=KidsCategory">View more</a>
-                    </figcaption>
-                </figure>
-            </div>
-
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-                <figure class="effect-ming tm-video-item">
-                    <img src="Assets/img/img-03.jpg" alt="Image" class="img-fluid">
-                    <figcaption class="d-flex align-items-center justify-content-center">
-                        <h2>Accessoires</h2>
-                        <a href="index.php?action=AccessoriesCategory">View more</a>
-                    </figcaption>
-                </figure>
-            </div>
-        </div>
-    </div>
 <?php
 $content = ob_get_clean();
 require 'gabarit.php';
