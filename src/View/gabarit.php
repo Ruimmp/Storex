dad3aaf18793eb82a02dfe92e8db4f02d8b219fb
Rui <61775818+Ruimmp@users.noreply.github.com>
2022-06-02 13:47:39 +0200

    :white_check_mark: | Correction de l'appel  des fichiers footer et jquery

--- src/View/gabarit.php
+++ src/View/gabarit.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="Assets/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="Assets/css/templatemo-style.css">
-    <link rel="stylesheet" href="Assets/css/styles/footer.css">
+    <link rel="stylesheet" href="Assets/styles/footer.css">
 
     <!-- Register CSS -->
     <link rel="stylesheet" href="Assets/styles/FormsRegister_Login.css">
@@ -172,7 +172,6 @@
 </div>
 <!-- End -->
 
-<script src="Assets/js/jquery.min.js"></script>
 <script src="Assets/js/plugins.js"></script>
 
 <script>
