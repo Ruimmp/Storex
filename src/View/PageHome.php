26537101b0c672056980f9a4f9d58aedc129a216
Rui <61775818+Ruimmp@users.noreply.github.com>
2022-06-11 04:25:34 +0200

    :pencil2: | Correction de l'affichage
    - L'affichage était en colonne tandis qu'il devrait être en ligne de 4 part 4

--- src/View/PageHome.php
+++ src/View/PageHome.php
@@ -12,7 +12,7 @@ $rows = 0; // Column count
 ?>
 
     <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll"
-         data-image-src="Assets/img/hero.jpg">
+         data-image-src="Assets/img/banner_search.png">
         <form class="d-flex tm-search-form">
             <input class="tm-search-input" type="search" placeholder="Cherchez ici" aria-label="Search">
             <button class="btn tm-search-btn" type="submit">
@@ -21,48 +21,33 @@ $rows = 0; // Column count
         </form>
     </div>
 
-<?php $articlesResults = displayArticles();
-
-foreach ($articlesResults as $result) : ?>
-
-    <?php $rows++; ?>
-    <?php if ($rows % 4) : // tests to have 4 items / line ?>
-
-        <div class="container-fluid tm-container-content tm-mt-60">
-
-        <?php $rows = 0; ?>
-    <?php endif ?>
-
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-        <div class="card h-100">
-            <img class="card-img-top" src="<?= $result['Image']; ?>"
-                 alt="<?= $result['Name']; ?>"/>
-
-            <!-- Product details-->
-            <div class="card-body p-4">
-                <div class="text-center">
-                    <!-- Product name-->
-                    <h5 class="fw-bolder"><?= $result['Name']; ?></h5>
-                    <!-- Product price-->
-                    Prix: <?= $result['Price']; ?> CHF
+<?php $articlesResults = displayArticles(); ?>
+    <div class="container-fluid tm-container-content tm-mt-60">
+        <div class="row tm-gallery">
+            <?php foreach ($articlesResults as $result) : ?>
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 tm-mb-90">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="<?= $result['Image']; ?>" alt="<?= $result['Name']; ?>"
+                             style="max-width:100%"/>
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder"><?= $result['Name']; ?></h5>
+                                <!-- Product price-->
+                                Prix: <?= $result['Price']; ?> CHF
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="form-group1 form-button">
+                            <input type="submit" name="signup" id="signup" class="form-submit"
+                                   value="Voir les détails"/>
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <!-- Product actions-->
-            <div class="form-group1 form-button">
-                <input type="submit" name="signup" id="signup" class="form-submit" value="Ajouter au panier"/>
-            </div>
-
+            <?php endforeach ?>
         </div>
     </div>
-
-    <?php if ($rows % 4) : ?>
-
-        </div>
-
-    <?php endif ?>
-<?php endforeach ?>
-
 <?php
 $content = ob_get_clean();
 require 'gabarit.php';
