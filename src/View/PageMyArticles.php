58dfbb7ffc8bc9647ee6bd9332a92ecaa1bd7c93
Rui <61775818+Ruimmp@users.noreply.github.com>
2022-06-14 16:16:53 +0200

    :pencil2: | Mise à jour du tableau pour l'affichage des annonces de l'utilisateur

--- src/View/PageMyArticles.php
+++ src/View/PageMyArticles.php
@@ -19,11 +19,11 @@ $rows = 0; // Column count
                 <table class="table table-striped custom-table">
                     <thead>
                     <tr>
-                        <th class="tm-text-gray-dark mb-3" scope="col">ID (annonce)</th>
-                        <th class="tm-text-gray-dark mb-3" scope="col">Nom (annonce)</th>
-                        <th class="tm-text-gray-dark mb-3" scope="col">Prix (annonce)</th>
-                        <th class="tm-text-gray-dark mb-3" scope="col">Description (annonce)</th>
-                        <th class="tm-text-gray-dark mb-3" scope="col">Image (annonce)</th>
+                        <th class="tm-text-gray-dark mb-3" scope="col">ID</th>
+                        <th class="tm-text-gray-dark mb-3" scope="col">Nom</th>
+                        <th class="tm-text-gray-dark mb-3" scope="col">Prix</th>
+                        <th class="tm-text-gray-dark mb-3" scope="col">Description</th>
+                        <th class="tm-text-gray-dark mb-3" scope="col">Image</th>
                         <th class="tm-text-gray-dark mb-3" scope="col"></th>
                         <th class="tm-text-gray-dark mb-3" scope="col"></th>
                     </tr>
