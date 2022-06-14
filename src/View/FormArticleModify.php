e86b9731c18a31456cdabab58f97b4529a603d86
Rui <61775818+Ruimmp@users.noreply.github.com>
2022-06-14 16:16:14 +0200

    :pencil2: | Création et affichage de la modification d'annonce

--- src/View/FormArticleModify.php
+++ src/View/FormArticleModify.php
@@ -11,31 +11,67 @@ ob_start();
 ?>
 
 
+<?php foreach ($announcesResults as $result) : ?>
 
     <section class="signup">
         <div class="container">
             <div class="signup-content">
                 <div class="signup-form">
-                    <h2 class="form-title">Connectez vous</h2>
+                    <h2 class="form-title">Modification de <?= $result['Name']; ?></h2>
+                    <form method="POST" class="register-form" id="register-form" action="index.php?action=modifyArticle">
 
-                    <?php foreach ($announcesResults as $result) : ?>
-                        <form method="POST" class="register-form" id="register-form"
-                              action="index.php?action=modifyArticle">
+                        <div class="form-group">
+                            <label><i class="zmdi zmdi-code"></i></label>
+                            <input
+                                    type="text"
+                                    name="articleName"
+                                    id="articleName"
+                                    value="<?= $result['ID']; ?>"
+                                    disabled
+                                    required/>
+                        </div>
 
-                            <div class="form-group">
-                                <label><i class="zmdi zmdi-code"></i></label>
-                                <input
-                                        type="text"
-                                        name="articleName"
-                                        id="articleName"
-                                        value="<?= $result['Name']; ?>"
-                                        disabled
-                                        required/>
-                            </div>
-                        </form>
+                        <div class="form-group">
+                            <label><i class="zmdi zmdi-code"></i></label>
+                            <input
+                                    type="text"
+                                    name="articleName"
+                                    id="articleName"
+                                    value="<?= $result['Name']; ?>"
+                                    required/>
+                        </div>
 
-                    <?php endforeach ?>
+                        <div class="form-group">
+                            <label><i class="zmdi zmdi-code"></i></label>
+                            <input
+                                    type="text"
+                                    name="articleName"
+                                    id="articleName"
+                                    value="<?= $result['Price']; ?>"
+                                    required/>
+                        </div>
 
+                        <div class="form-group">
+                            <label><i class="zmdi zmdi-code"></i></label>
+                            <input
+                                    type="text"
+                                    name="articleName"
+                                    id="articleName"
+                                    value="<?= $result['Description']; ?>"
+                                    required/>
+                        </div>
+
+                        <div class="form-group">
+                            <label><i class="zmdi zmdi-code"></i></label>
+                            <input
+                                    type="text"
+                                    name="articleName"
+                                    id="articleName"
+                                    value="<?= $result['Image']; ?>"
+                                    required/>
+                        </div>
+
+                    </form>
 
                     <div class="form-group form-button">
                         <input type="submit" name="signup" id="signup" class="form-submit" value="Enregistrer"/>
@@ -43,8 +79,9 @@ ob_start();
                 </div>
             </div>
         </div>
-
     </section>
+
+<?php endforeach ?>
 
 <?php
 $content = ob_get_clean();
