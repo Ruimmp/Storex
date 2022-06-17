087a933841e82da4876bf4315e6d19a4a3771d31
Rui <61775818+Ruimmp@users.noreply.github.com>
2022-06-17 14:45:31 +0200

    :pencil2: | Page pour afficher les détails d'annonce

--- src/View/PageArticleDetails.php
+++ src/View/PageArticleDetails.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @file        ArticleDetails.php
+ * @file        PageArticleDetails.php
  * @brief       This view is designed to display the edit article form
- * @author      Created by Monteiro.Rui
- * @version     12.06.2022
+ * @author      Created by Pereira.Nuno
+ * @version     16.06.2022
  */
 
 $title = "Storex | Détails d'annonce";
@@ -29,8 +29,9 @@ ob_start();
                         <?= $result['Description']; ?>
                     </p>
                     <div class="text-center mb-4 btn-dtl">
-                        <a href="mailto:<?= $result['Email']; ?>" class="form-submit">Contacter
-                            l'annonceur</a>
+                        <a href="index.php?action=FormContact&ID=<?= $result['ID']; ?>"
+                           class="form-submit btn btn-primary tm-btn-big">Contacter l'annonceur
+                        </a>
                     </div>
 
                     <div class="mb-4 d-flex flex-wrap justify-content-center text-xxl-center">
@@ -48,42 +49,6 @@ ob_start();
     </div>
 
 <?php endforeach ?>
-
-    <!--
-<?php $articlesResults = displayAdminArticles(); ?>
-<?php foreach ($announcesResults as $result) : ?>
-    <?php if (isset($_SESSION['Email']) == $result['Email']) : ?>
-
-        <div class="row mb-4 container">
-            <h2 class="col-12 tm-text-primary text-xxl-center">
-                <span class="tm-text-gray-dark">Autres annonces par: </span>
-                <a href="mailto:<?= $result['Email']; ?>" class="lien">
-                    <span><?= $result['Firstname']; ?></span>
-                </a>
-            </h2>
-        </div>
-        <div class="row mb-3 tm-gallery container">
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
-                <div class="h-90 tm-mt-10">
-                    <img class="card-img-top imgedit-dtl" src="<?= $result['Image']; ?>" alt="<?= $result['Name']; ?>"/>
-                    <div class="p-4">
-                        <div class="text-center">
-                            <h4><?= $result['Name']; ?></h4>
-                            Prix: <?= $result['Price']; ?> CHF
-                        </div>
-                    </div>
-                    <div class="form-group1 form-button btn-dtl">
-                        <a href="index.php?action=PageArticleDetails&ID=<?= $result['ID']; ?>">
-                            <input type="submit" class="form-submit" value="Voir les détails"/>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php endif; ?>
-
-<?php endforeach ?>
--->
 
 <?php
 $content = ob_get_clean();
